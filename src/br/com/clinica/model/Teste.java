037961092a14f52b95be0654c70a417f95c22de8
Natanael <natanaela.neto@unifacol.edu.br>
2020-10-10 17:40:20 -0300

    pasta do projeto

---
+++ src/br/com/clinica/model/Teste.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.clinica.model;
+
+/**
+ *
+ * @author nneto
+ */
+public class Teste {
+    public int id;
+    public String nome;
+    public String idade;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getIdade() {
+        return idade;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(String idade) {
+        this.idade = idade;
+    }
+    
+    
+}
